b065057e0ae7e52ba0e53ffddce069cab0321d33
austinc <ac6404@gmail.com>
2021-02-20 21:36:14 -0600

    add versioning to endpoints

--- app.js
+++ app.js
@@ -17,10 +17,6 @@ db.once('open', () => console.log('connected to database'))
 
 var app = express();
 
-// view engine setup
-//~ app.set('views', path.join(__dirname, 'views'));
-//~ app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,8 +24,8 @@ app.use(cookieParser());
 //~ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/locations', locationsRouter);
-app.use('/channels', channelsRouter); 
+app.use('v1/locations', locationsRouter);
+app.use('v1/channels', channelsRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
